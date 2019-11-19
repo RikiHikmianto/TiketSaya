231689fff2b03d367ada281bc609c5818a861c23
Riki Hikmianto <dayatketangga95@gmail.com>
2019-11-19 20:59:01 +0700

    animation splash and get started

--- app/src/main/java/com/akokom/irajayatiket/GetStartedAct.java
+++ app/src/main/java/com/akokom/irajayatiket/GetStartedAct.java
@@ -5,18 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GetStartedAct extends AppCompatActivity {
 
     Button btn_sign_in, btn_new_account_create;
+    Animation toptobottomalpha, bottomtotopalpha;
+    ImageView emblem_app;
+    TextView intro_app;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
 
+        //load animation
+        toptobottomalpha = AnimationUtils.loadAnimation(this, R.anim.toptobottomalpha);
+        bottomtotopalpha = AnimationUtils.loadAnimation(this, R.anim.bottomtotopalpha);
+
+        intro_app = findViewById(R.id.intro_app);
+        emblem_app = findViewById(R.id.emblem_app);
         btn_sign_in = findViewById(R.id.btn_sign_in);
         btn_new_account_create = findViewById(R.id.btn_new_account_create);
+
+        //run animation
+        emblem_app.startAnimation(toptobottomalpha);
+        intro_app.startAnimation(toptobottomalpha);
+        btn_sign_in.startAnimation(bottomtotopalpha);
+        btn_new_account_create.startAnimation(bottomtotopalpha);
+
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/akokom/irajayatiket/SplashAct.java
+++ app/src/main/java/com/akokom/irajayatiket/SplashAct.java
@@ -6,24 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashAct extends AppCompatActivity {
+
+    Animation app_splash, bottomtotopalpha;
+    ImageView app_logo;
+    TextView app_subtittle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-//        setting timer untuk 1 detik
+        //load animation
+        app_splash = AnimationUtils.loadAnimation(this, R.anim.app_splash);
+        bottomtotopalpha = AnimationUtils.loadAnimation(this, R.anim.bottomtotopalpha);
+
+        //load element
+        app_logo = findViewById(R.id.app_logo);
+        app_subtittle = findViewById(R.id.app_subtittle);
+
+        //run animation
+        app_logo.startAnimation(app_splash);
+        app_subtittle.startAnimation(bottomtotopalpha);
+
+        //setting timer untuk 1 detik
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-//                merubah activity ke activity lain
+                //merubah activity ke activity lain
                 Intent gogetstarted = new Intent(SplashAct.this, GetStartedAct.class);
                 startActivity(gogetstarted);
                 finish();
             }
-        }, 1000); /*1000 ms = 1 s*/
+        }, 2000); /*1000 ms = 1 s*/
     }
 }
